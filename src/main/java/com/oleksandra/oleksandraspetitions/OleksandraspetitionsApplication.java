31d94ac5bbf86de358f762b22eb431cdf4dbbb73
Oleksandra <ag7756862@gmail.com>
2026-04-18 01:16:21 +0100

    Initial Spring Boot project setup with Java 21 and WAR packaging

---
+++ src/main/java/com/oleksandra/oleksandraspetitions/OleksandraspetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.oleksandra.oleksandraspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OleksandraspetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OleksandraspetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oleksandra/oleksandraspetitions/OleksandraspetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oleksandra.oleksandraspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OleksandraspetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
